9b83c0b55d4017ef66387dd2f38bcb2ff0167442
Yogesh Rai <69380654+yogesh-rai@users.noreply.github.com>
2022-03-22 23:26:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HashRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from "./pages/Home";
 import SearchPage from './pages/SearchPage';
 
