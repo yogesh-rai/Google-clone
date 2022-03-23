0774393eaefdfecd7ee5ce0fa0d30f1d29a1f9b0
Yogesh Rai <69380654+yogesh-rai@users.noreply.github.com>
2022-03-23 17:27:02 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import "./Home.css";
 import AppsIcon from "@material-ui/icons/Apps";
 import { Avatar } from "@material-ui/core";
 import Search from "../components/Search";
+import logo from "../google_logo.png";
 
 function Home() {
   return (
@@ -23,7 +24,7 @@ function Home() {
 
       <div className='home_body'>
           <img 
-           src='https://png2.cleanpng.com/sh/ebc4ae0e67202242909c0a6dabdb6246/L0KzQYm3VsIzN5hqjJH0aYP2gLBuTfdwd5hxfZ91b3fyPbj2jBdtbaFxfeo2Z3Byd73sTgNmaaNogJ8AYkLmdIjthMY3aWoASpC6NEW6RIO8VME2Omo7TqcAM0O3QoG8TwBvbz==/kisspng-google-logo-googleplex-google-search-5b2cd7fd66a992.1457425415296655334205.png'
+           src={logo}
             alt="google-logo" />
           <div className='input'>
             <Search />
